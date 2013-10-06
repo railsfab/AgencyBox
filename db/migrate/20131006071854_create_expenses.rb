eb520e9f565b9ec71910896a33e97a07a0f36931
Bhaskar <bhaskar8088@gmail.com>
2013-10-06 13:07:27 +0530

    Added model Expense

---
+++ db/migrate/20131006071854_create_expenses.rb
@@ -0,0 +1,14 @@
+class CreateExpenses < ActiveRecord::Migration
+  def change
+    create_table :expenses do |t|
+      t.integer :user_id
+      t.interger :category_id
+      t.decimal :amount
+      t.boolean :status
+      t.boolean :rejected
+      t.string :description
+      
+      t.timestamps
+    end
+  end
+end
