f3d7aa0bdf9811b55df9f08af135401f08eed9ca
Bhaskar <bhaskar8088@gmail.com>
2013-10-07 05:02:32 +0530

    Added some validations for expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,11 @@
 class Expense < ActiveRecord::Base
+    belongs_to :category, class_name: :ExpenseCategory, foreign_key: :category_id
+    belongs_to :user
+
+    validates :amount, presence: true
+    validates :description, presence: true, length: { :minimum => 5 }
+
+    def categories
+        ExpenseCategory.pluck(:name, :id)
+    end
 end
