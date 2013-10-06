78d64781746c7ab3abb094db4d8d5fb7f0b2adc3
Bhaskar <bhaskar8088@gmail.com>
2013-10-06 13:09:13 +0530

    TYpos

--- db/migrate/20131006071854_create_expenses.rb
+++ db/migrate/20131006071854_create_expenses.rb
@@ -2,12 +2,12 @@ class CreateExpenses < ActiveRecord::Migration
   def change
     create_table :expenses do |t|
       t.integer :user_id
-      t.interger :category_id
+      t.integer :category_id
       t.decimal :amount
       t.boolean :status
       t.boolean :rejected
       t.string :description
-      
+
       t.timestamps
     end
   end
