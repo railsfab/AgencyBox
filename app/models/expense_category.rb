9abfc16e15873a4012713338f06d43c0a76a3af5
Bhaskar <bhaskar8088@gmail.com>
2013-10-07 00:10:49 +0530

    Added valdiations for expense category

--- app/models/expense_category.rb
+++ app/models/expense_category.rb
@@ -1,2 +1,10 @@
 class ExpenseCategory < ActiveRecord::Base
+    validates :name, presence: true, length: { :minimum => 5, :maximum => 30 }
+    validates :description, presence: true, length: { :minimum => 5 }
+    validate :create_and_validate_slug
+    validates :slug, :uniqueness => { message: ": Category already exists, please choose another category" }
+
+    def create_and_validate_slug
+        self.slug = self.name.parameterize
+    end
 end
