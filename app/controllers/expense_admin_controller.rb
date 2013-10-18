04417378cd03b6249e04c52df6202a32b89a2a93
Bhaskar <bhaskar8088@gmail.com>
2013-10-18 10:43:40 +0530

    Expense admin - filtering

--- app/controllers/expense_admin_controller.rb
+++ app/controllers/expense_admin_controller.rb
@@ -1,6 +1,23 @@
 class ExpenseAdminController < ApplicationController
     def index
         @expenses = Expense.all
+        if params.include?("status")
+            if params['status'] == "pending"
+                @expenses = @expenses.where({ :status => false, :rejected => false })
+            elsif params["status"] == "approved"
+                @expenses = @expenses.where({ :status => true, :rejected => false })
+            elsif params["status"] == "rejected"
+                @expenses = @expenses.where({ :status => false, :rejected => true })
+            end
+        end
+
+        if params.include?(:amount_filter)
+            if params[:amount_filter] == "asc"
+                @expenses = @expenses.order("amount ASC")
+            elsif params[:amount_filter] == "desc"
+                @expenses = @expenses.order("amount desc")
+            end
+        end
     end
 
     def edit
