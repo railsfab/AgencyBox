9c84897c4da8d4efb7d804ad1d2b4b3b378c6747
Bhaskar <bhaskar8088@gmail.com>
2013-10-19 11:02:42 +0530

    Authorize admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  #authorize_resource For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  before_filter authorize_resource
-  check_authorization
+    # Prevent CSRF attacks by raising an exception.
+    #authorize_resource For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    #before_filter authorize_resource
+    #check_authorization
+
+    before_filter :authorize_admin
+
+    def not_found
+        raise ActionController::RoutingError.new('Not Found')
+    end
+
+    protected
+
+    def authorize_admin
+        if request.fullpath.index("/admin") == 0
+            if not current_user.has_role? :admin
+                not_found
+            end
+        end
+    end
 end
