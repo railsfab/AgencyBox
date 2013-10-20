07e790c4886b9dfa16b45dc9c85ced72f6e22827
Bhaskar <bhaskar8088@gmail.com>
2013-10-20 19:39:03 +0530

    Added model Project and its migration

---
+++ app/models/project.rb
@@ -0,0 +1,15 @@
+class Project < ActiveRecord::Base
+    belongs_to :created_by, class_name: :User, foreign_key: :created_by_id
+
+    #validations
+    validates :name, presence: true, length: { :minimum => 5 }
+    validates :description, presence: true, length: { :minimum => 5 }
+    validate :create_and_validate_slug
+    validates :slug, :uniqueness => { 
+        message: ": Project with this name already exists, 
+        please choose another category" }
+
+    def create_and_validate_slug
+        self.slug = self.name.parameterize
+    end
+end
